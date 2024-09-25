chinchans/oai5g

Add new F1AP msg enc&dec library
Add a new target f1ap_lib that groups F1AP ASN.1 encoding&decoding for
messages defined f1ap_messages_types.h. The objective is to, for each
F1AP message,
- add encoder&decoder (similar to what already exists, but without
  mandatory message sending afterwards)
- eq_X() function that checks two messages for equality (to be used
  mostly in tests)
- cp_X() function that deep-copies a message, e.g., to be used when
  sending ITTI messages, instead of manual copying done currently
- free_X() function that frees the memory associated to a memory, to
  avoid memory leaks.

Each message will be tested, notably that encoding&decoding as well as
copying yield identical messages.

The actual message encoding & decoding, tests, and their use will be
done in the forthcoming commits.

openair2/F1AP/lib/f1ap_lib_common.c
@@ -1,0 +1,41 @@
+/*
+ * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The OpenAirInterface Software Alliance licenses this file to You under
+ * the OAI Public License, Version 1.1  (the "License"); you may not use this file
+ * except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.openairinterface.org/?page_id=698
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *-------------------------------------------------------------------------------
+ * For more information about the OpenAirInterface (OAI) Software Alliance:
+ *      contact@openairinterface.org
+ */
+
+#include "f1ap_lib_common.h"
+#include "f1ap_messages_types.h"
+
+#include "OCTET_STRING.h"
+
+#include "common/utils/assertions.h"
+
+bool eq_f1ap_plmn(const f1ap_plmn_t *a, const f1ap_plmn_t *b)
+{
+  return a->mcc == b->mcc && a->mnc == b->mnc && a->mnc_digit_length == b->mnc_digit_length;
+}
+
+uint8_t *cp_octet_string(const OCTET_STRING_t *os, int *len)
+{
+  uint8_t *buf = calloc(os->size, sizeof(*buf));
+  AssertFatal(buf != NULL, "out of memory\n");
+  memcpy(buf, os->buf, os->size);
+  *len = os->size;
+  return buf;
+}

jessest94106/openairinterface5g

openair2/F1AP/lib/f1ap_lib_common.c
@@ -1,0 +1,41 @@
+/*
+ * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The OpenAirInterface Software Alliance licenses this file to You under
+ * the OAI Public License, Version 1.1  (the "License"); you may not use this file
+ * except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.openairinterface.org/?page_id=698
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *-------------------------------------------------------------------------------
+ * For more information about the OpenAirInterface (OAI) Software Alliance:
+ *      contact@openairinterface.org
+ */
+
+#include "f1ap_lib_common.h"
+#include "f1ap_messages_types.h"
+
+#include "OCTET_STRING.h"
+
+#include "common/utils/assertions.h"
+#include "common/utils/utils.h"
+
+bool eq_f1ap_plmn(const f1ap_plmn_t *a, const f1ap_plmn_t *b)
+{
+  return a->mcc == b->mcc && a->mnc == b->mnc && a->mnc_digit_length == b->mnc_digit_length;
+}
+
+uint8_t *cp_octet_string(const OCTET_STRING_t *os, int *len)
+{
+  uint8_t *buf = calloc_or_fail(os->size, sizeof(*buf));
+  memcpy(buf, os->buf, os->size);
+  *len = os->size;
+  return buf;
+}