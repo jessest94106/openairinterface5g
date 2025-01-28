saffanazyan07/oai_w-agf

Refactor set_tdd_config_nr to use frame_structure_t and tdd_period_config_t
* assuming num_dl_slots and num_ul_slots are including also
  slots with dl/ul symbols respectively
* refactor set_tdd_config_nr
* moved function to MAC
* fill max_num_of_symbol_per_slot_list depending on slot type
* use new set_tdd_config_nr in stack
* minor refactor in config.c to improve readability (scs)

Co-authored-by: Guido Casati <guido.casati@firecell.io>

openair1/SCHED_NR/phy_frame_config_nr.c
@@ -35,120 +35,109 @@
 #include "SCHED_NR/phy_frame_config_nr.h"
 #include "common/utils/nr/nr_common.h"
 
-/*******************************************************************
-*
-* NAME :         set_tdd_configuration
-*
-* PARAMETERS :   pointer to frame configuration
-*
-* OUTPUT:        table of uplink symbol for each slot for 2 frames
-*
-* RETURN :       nb_periods_per_frame if tdd has been properly configurated
-*                -1 tdd configuration can not be done
-*
-* DESCRIPTION :  generate bit map for uplink symbol for each slot for several frames
-*                see TS 38.213 11.1 Slot configuration
-*
-*********************************************************************/
-
-int set_tdd_config_nr(nfapi_nr_config_request_scf_t *cfg,
-                      int mu,
-                      int nrofDownlinkSlots,
-                      int nrofDownlinkSymbols,
-                      int nrofUplinkSlots,
-                      int nrofUplinkSymbols)
+/** @brief This function processes TDD dedicated configuration for NR
+ *         by processing the tdd_slot_bitmap and period_cfg, and
+ *         allocates memory and fills max_num_of_symbol_per_slot_list
+ *         in the nfapi config request (cfg)
+ *  @ref   see TS 38.213 11.1 Slot configuration
+ *  @param cfg NR config request structure pointer
+ *  @param fs TDD configuration pointer
+ *  @returns void
+ */
+void set_tdd_config_nr(nfapi_nr_config_request_scf_t *cfg, frame_structure_t *fs)
 {
+  AssertFatal(fs != NULL, "frame_structure must be non-NULL");
 
   int slot_number = 0;
-  int nb_periods_per_frame = get_nb_periods_per_frame(cfg->tdd_table.tdd_period.value);
-  int nb_slots_to_set = TDD_CONFIG_NB_FRAMES*(1<<mu)*NR_NUMBER_OF_SUBFRAMES_PER_FRAME;
+  int nb_slots_to_set = TDD_CONFIG_NB_FRAMES * fs->numb_slots_frame;
+  int slot_index = 0;
+  tdd_period_config_t *pc = &fs->period_cfg;
+  tdd_bitmap_t *tdd_slot_bitmap = pc->tdd_slot_bitmap;
 
-  int nb_slots_per_period = ((1<<mu) * NR_NUMBER_OF_SUBFRAMES_PER_FRAME)/nb_periods_per_frame;
+  LOG_I(NR_PHY,
+        "Set TDD Period Configuration: %d periods per frame, %d slots to be configured (%d DL, %d UL)\n",
+        fs->numb_period_frame,
+        nb_slots_to_set,
+        pc->num_dl_slots,
+        pc->num_ul_slots);
 
-  if ( (nrofDownlinkSymbols + nrofUplinkSymbols) == 0 )
-    AssertFatal(nb_slots_per_period == (nrofDownlinkSlots + nrofUplinkSlots),
-                "set_tdd_configuration_nr: given period is inconsistent with current tdd configuration, nrofDownlinkSlots %d, nrofUplinkSlots %d, nb_slots_per_period %d \n",
-                nrofDownlinkSlots,nrofUplinkSlots,nb_slots_per_period);
-  else {
-    AssertFatal(nrofDownlinkSymbols + nrofUplinkSymbols < 14,"illegal symbol configuration DL %d, UL %d\n",nrofDownlinkSymbols,nrofUplinkSymbols);
-    AssertFatal(nb_slots_per_period == (nrofDownlinkSlots + nrofUplinkSlots + 1),
-                "set_tdd_configuration_nr: given period is inconsistent with current tdd configuration, nrofDownlinkSlots %d, nrofUplinkSlots %d, nrofMixed slots 1, nb_slots_per_period %d \n",
-                nrofDownlinkSlots,nrofUplinkSlots,nb_slots_per_period);
+  // Allocate memory formax_num_of_symbol_per_slot_list items
+  cfg->tdd_table.max_tdd_periodicity_list = calloc_or_fail(nb_slots_to_set, sizeof(nfapi_nr_max_tdd_periodicity_t));
+  for (int i = 0; i < nb_slots_to_set; i++) {
+    nfapi_nr_max_tdd_periodicity_t *l = &cfg->tdd_table.max_tdd_periodicity_list[i];
+    l->max_num_of_symbol_per_slot_list = calloc_or_fail(NR_NUMBER_OF_SYMBOLS_PER_SLOT, sizeof(*l->max_num_of_symbol_per_slot_list));
   }
-
-  cfg->tdd_table.max_tdd_periodicity_list = (nfapi_nr_max_tdd_periodicity_t *) malloc(nb_slots_to_set*sizeof(nfapi_nr_max_tdd_periodicity_t));
-
-  for(int memory_alloc =0 ; memory_alloc<nb_slots_to_set; memory_alloc++)
-    cfg->tdd_table.max_tdd_periodicity_list[memory_alloc].max_num_of_symbol_per_slot_list = (nfapi_nr_max_num_of_symbol_per_slot_t *) malloc(NR_NUMBER_OF_SYMBOLS_PER_SLOT*sizeof(
-          nfapi_nr_max_num_of_symbol_per_slot_t));
-
-  while(slot_number != nb_slots_to_set) {
-    if(nrofDownlinkSlots != 0) {
-      for (int number_of_symbol = 0; number_of_symbol < nrofDownlinkSlots*NR_NUMBER_OF_SYMBOLS_PER_SLOT; number_of_symbol++) {
-        cfg->tdd_table.max_tdd_periodicity_list[slot_number].max_num_of_symbol_per_slot_list[number_of_symbol%NR_NUMBER_OF_SYMBOLS_PER_SLOT].slot_config.value= 0;
-
-        if((number_of_symbol+1)%NR_NUMBER_OF_SYMBOLS_PER_SLOT == 0)
+  // Loop through all periods to be configured
+  while (slot_number != nb_slots_to_set) {
+    slot_index = get_slot_idx_in_period(slot_number, fs);
+    /* Fill max_tdd_periodicity_list */
+    nfapi_nr_max_tdd_periodicity_t *list = &cfg->tdd_table.max_tdd_periodicity_list[slot_number];
+    // FULL DOWNLINK SLOTS
+    if (tdd_slot_bitmap[slot_index].slot_type == TDD_NR_DOWNLINK_SLOT) {
+      for (int number_of_symbol = 0; number_of_symbol < NR_NUMBER_OF_SYMBOLS_PER_SLOT; number_of_symbol++) {
+        list->max_num_of_symbol_per_slot_list[number_of_symbol % NR_NUMBER_OF_SYMBOLS_PER_SLOT].slot_config.value = 0;
+        if ((number_of_symbol + 1) % NR_NUMBER_OF_SYMBOLS_PER_SLOT == 0) {
           slot_number++;
+          list += 1;
+        }
       }
     }
-
-    if (nrofDownlinkSymbols != 0 || nrofUplinkSymbols != 0) {
-      for(int number_of_symbol =0; number_of_symbol < nrofDownlinkSymbols; number_of_symbol++) {
-        cfg->tdd_table.max_tdd_periodicity_list[slot_number].max_num_of_symbol_per_slot_list[number_of_symbol].slot_config.value= 0;
+    // FLEXIBLE SLOTS
+    if (tdd_slot_bitmap[slot_index].slot_type == TDD_NR_MIXED_SLOT) {
+      int nrofDLSymbolsInSlot = tdd_slot_bitmap[slot_index].num_dl_symbols;
+      int nrofULSymbolsInSlot = tdd_slot_bitmap[slot_index].num_ul_symbols;
+      AssertFatal(nrofDLSymbolsInSlot + nrofULSymbolsInSlot < NR_NUMBER_OF_SYMBOLS_PER_SLOT,
+                  "illegal symbol configuration DL %d, UL %d\n",
+                  nrofDLSymbolsInSlot,
+                  nrofULSymbolsInSlot);
+      // DL Symbols
+      for (int number_of_symbol = 0; number_of_symbol < nrofDLSymbolsInSlot; number_of_symbol++) {
+        list->max_num_of_symbol_per_slot_list[number_of_symbol].slot_config.value = 0;
       }
-
-      for(int number_of_symbol = nrofDownlinkSymbols; number_of_symbol < NR_NUMBER_OF_SYMBOLS_PER_SLOT-nrofUplinkSymbols; number_of_symbol++) {
-        cfg->tdd_table.max_tdd_periodicity_list[slot_number].max_num_of_symbol_per_slot_list[number_of_symbol].slot_config.value= 2;
+      // Flexible Symbols
+      for (int number_of_symbol = nrofDLSymbolsInSlot; number_of_symbol < NR_NUMBER_OF_SYMBOLS_PER_SLOT - nrofULSymbolsInSlot;
+           number_of_symbol++) {
+        list->max_num_of_symbol_per_slot_list[number_of_symbol].slot_config.value = 2;
       }
-
-      for(int number_of_symbol = NR_NUMBER_OF_SYMBOLS_PER_SLOT-nrofUplinkSymbols; number_of_symbol < NR_NUMBER_OF_SYMBOLS_PER_SLOT; number_of_symbol++) {
-        cfg->tdd_table.max_tdd_periodicity_list[slot_number].max_num_of_symbol_per_slot_list[number_of_symbol].slot_config.value= 1;
+      // UL Symbols
+      for (int number_of_symbol = NR_NUMBER_OF_SYMBOLS_PER_SLOT - nrofULSymbolsInSlot;
+           number_of_symbol < NR_NUMBER_OF_SYMBOLS_PER_SLOT;
+           number_of_symbol++) {
+        list->max_num_of_symbol_per_slot_list[number_of_symbol].slot_config.value = 1;
       }
-
       slot_number++;
+      list += 1;
     }
-
-    if(nrofUplinkSlots != 0) {
-      for (int number_of_symbol = 0; number_of_symbol < nrofUplinkSlots*NR_NUMBER_OF_SYMBOLS_PER_SLOT; number_of_symbol++) {
-        cfg->tdd_table.max_tdd_periodicity_list[slot_number].max_num_of_symbol_per_slot_list[number_of_symbol%NR_NUMBER_OF_SYMBOLS_PER_SLOT].slot_config.value= 1;
-
-        if((number_of_symbol+1)%NR_NUMBER_OF_SYMBOLS_PER_SLOT == 0)
+    // FULL UPLINK SLOTS
+    if (tdd_slot_bitmap[slot_index].slot_type == TDD_NR_UPLINK_SLOT) {
+      for (int number_of_symbol = 0; number_of_symbol < NR_NUMBER_OF_SYMBOLS_PER_SLOT; number_of_symbol++) {
+        list->max_num_of_symbol_per_slot_list[number_of_symbol % NR_NUMBER_OF_SYMBOLS_PER_SLOT].slot_config.value = 1;
+        if ((number_of_symbol + 1) % NR_NUMBER_OF_SYMBOLS_PER_SLOT == 0) {
           slot_number++;
+          list += 1;
+        }
       }
     }
   }
-
-  /*
-  while(slot_number != nb_slots_to_set) {
-    for (int number_of_slot = 0; number_of_slot < nrofDownlinkSlots; number_of_slot++) {
-      frame_parms->tdd_uplink_nr[slot_number] = NR_TDD_DOWNLINK_SLOT;
-      printf("slot %d set as downlink\n",slot_number);
-      slot_number++;
-    }
-
-    if (nrofDownlinkSymbols != 0 || nrofUplinkSymbols != 0) {
-       frame_parms->tdd_uplink_nr[slot_number] = (1<<nrofUplinkSymbols) - 1;
-       printf("slot %d set as SL\n",slot_number);
-       slot_number++;
-    }
-
-    for (int number_of_slot = 0; number_of_slot < nrofUplinkSlots; number_of_slot++) {
-      frame_parms->tdd_uplink_nr[slot_number] = NR_TDD_UPLINK_SLOT;
-      printf("slot %d set as uplink\n",slot_number);
-      slot_number++;
-    }
-
-    if (p_tdd_ul_dl_configuration->nrofUplinkSymbols != 0) {
-      LOG_E(PHY,"set_tdd_configuration_nr: uplink symbol for slot is not supported for tdd configuration \n");
-      return (-1);
+  // Dump 1 period on the output
+  for (int s = 0; s < fs->numb_slots_period; s++) {
+    if (tdd_slot_bitmap[s].slot_type == TDD_NR_MIXED_SLOT) {
+      char flexi_slot[NR_NUMBER_OF_SYMBOLS_PER_SLOT + 1];
+      memset(flexi_slot, 0, sizeof(flexi_slot));
+      for (int symb = 0; symb < NR_NUMBER_OF_SYMBOLS_PER_SLOT; symb++) {
+        uint8_t val = cfg->tdd_table.max_tdd_periodicity_list[s].max_num_of_symbol_per_slot_list[symb].slot_config.value;
+        char *symb_type_s[] = {"D", "U", "F"};
+        flexi_slot[symb] = *symb_type_s[val];
+      }
+      flexi_slot[NR_NUMBER_OF_SYMBOLS_PER_SLOT] = '\0';
+      LOG_I(NR_PHY, "TDD period configuration: slot %d is FLEXIBLE: %s\n", s, flexi_slot);
+    } else {
+      LOG_I(NR_PHY,
+            "TDD period configuration: slot %d is %s\n",
+            s,
+            tdd_slot_bitmap[s].slot_type == TDD_NR_DOWNLINK_SLOT ? "DOWNLINK" : "UPLINK");
     }
   }
-
-  if (frame_parms->p_tdd_UL_DL_ConfigurationCommon2 != NULL) {
-    LOG_E(PHY,"set_tdd_configuration_nr: additionnal tdd configuration 2 is not supported for tdd configuration \n");
-    return (-1);
-  }*/
-  return (nb_periods_per_frame);
 }
 
 /*******************************************************************
@@ -415,3 +404,48 @@ void free_tdd_configuration_dedicated_nr(NR_DL_FRAME_PARMS *frame_parms) {
   }
 }
 
+void do_tdd_config_sim(PHY_VARS_gNB *gNB, int mu)
+{
+  frame_structure_t fs = {.is_tdd = true};
+  fs.numb_slots_frame = (1 << mu) * NR_NUMBER_OF_SUBFRAMES_PER_FRAME;
+  fs.numb_period_frame = get_nb_periods_per_frame(gNB->gNB_config.tdd_table.tdd_period.value);
+  fs.numb_slots_period = fs.numb_slots_frame / fs.numb_period_frame;
+  tdd_period_config_t *pc = &fs.period_cfg;
+  // Set DL/UL slots
+  switch (mu)
+  {
+  case 0:
+    pc->num_dl_slots = 3;
+    pc->num_ul_slots = 1;
+    break;
+  
+  case 1:
+    pc->num_dl_slots = 7;
+    pc->num_ul_slots = 2;
+    break;
+
+  case 3:
+    pc->num_dl_slots = 27;
+    pc->num_ul_slots = 12;
+    break;
+
+  default:
+    break;
+  }
+  for (int i = 0; i < pc->num_dl_slots; i++) {
+    pc->tdd_slot_bitmap[i].slot_type = TDD_NR_DOWNLINK_SLOT;
+    pc->tdd_slot_bitmap[i].num_dl_symbols = 0;
+    pc->tdd_slot_bitmap[i].num_ul_symbols = 0;
+  }
+  for (int i = pc->num_dl_slots; i < fs.numb_slots_period - pc->num_ul_slots; i++) {
+    pc->tdd_slot_bitmap[i].slot_type = TDD_NR_MIXED_SLOT;
+    pc->tdd_slot_bitmap[i].num_dl_symbols = 6;
+    pc->tdd_slot_bitmap[i].num_ul_symbols = 4;
+  }
+  for (int i = pc->num_ul_slots; i < fs.numb_slots_period; i++) {
+    pc->tdd_slot_bitmap[i].slot_type = TDD_NR_UPLINK_SLOT;
+    pc->tdd_slot_bitmap[i].num_dl_symbols = 0;
+    pc->tdd_slot_bitmap[i].num_ul_symbols = 0;
+  }
+  set_tdd_config_nr(&gNB->gNB_config, &fs);
+}

jessest94106/openairinterface5g

openair1/SCHED_NR/phy_frame_config_nr.c
@@ -44,108 +44,112 @@
   #define NR_TST_PHY_PRINTF(...)
 #endif
 
-/*******************************************************************
-*
-* NAME :         set_tdd_configuration
-*
-* PARAMETERS :   pointer to frame configuration
-*
-* OUTPUT:        table of uplink symbol for each slot for 2 frames
-*
-* RETURN :       nb_periods_per_frame if tdd has been properly configurated
-*                -1 tdd configuration can not be done
-*
-* DESCRIPTION :  generate bit map for uplink symbol for each slot for several frames
-*                see TS 38.213 11.1 Slot configuration
-*
-*********************************************************************/
-
-int set_tdd_config_nr(nfapi_nr_config_request_scf_t *cfg,
-                      int mu,
-                      int nrofDownlinkSlots,
-                      int nrofDownlinkSymbols,
-                      int nrofUplinkSlots,
-                      int nrofUplinkSymbols)
+/** @brief This function processes TDD dedicated configuration for NR
+ *         by processing the tdd_slot_bitmap and period_cfg, and
+ *         allocates memory and fills max_num_of_symbol_per_slot_list
+ *         in the nfapi config request (cfg)
+ *  @ref   see TS 38.213 11.1 Slot configuration
+ *  @param cfg NR config request structure pointer
+ *  @param fs TDD configuration pointer
+ *  @returns void
+ */
+void set_tdd_config_nr(nfapi_nr_config_request_scf_t *cfg, frame_structure_t *fs)
 {
+  AssertFatal(fs != NULL, "frame_structure must be non-NULL");
+
   int slot_number = 0;
-  int nb_periods_per_frame = get_nb_periods_per_frame(cfg->tdd_table.tdd_period.value);
-  int nb_slots_to_set = (1 << mu) * NR_NUMBER_OF_SUBFRAMES_PER_FRAME;
+  int nb_slots_to_set = fs->numb_slots_frame;
+  int slot_index = 0;
+  tdd_period_config_t *pc = &fs->period_cfg;
+  tdd_bitmap_t *tdd_slot_bitmap = pc->tdd_slot_bitmap;
 
-  int nb_slots_per_period = ((1 << mu) * NR_NUMBER_OF_SUBFRAMES_PER_FRAME) / nb_periods_per_frame;
+  LOG_I(NR_PHY,
+        "Set TDD Period Configuration: %d periods per frame, %d slots to be configured (%d DL, %d UL)\n",
+        fs->numb_period_frame,
+        nb_slots_to_set,
+        pc->num_dl_slots,
+        pc->num_ul_slots);
 
-  if ((nrofDownlinkSymbols + nrofUplinkSymbols) == 0)
-    AssertFatal(nb_slots_per_period == (nrofDownlinkSlots + nrofUplinkSlots),
-                "set_tdd_configuration_nr: given period is inconsistent with current tdd configuration, nrofDownlinkSlots %d, "
-                "nrofUplinkSlots %d, nb_slots_per_period %d \n",
-                nrofDownlinkSlots,
-                nrofUplinkSlots,
-                nb_slots_per_period);
-  else {
-    AssertFatal(nrofDownlinkSymbols + nrofUplinkSymbols < 14,
-                "illegal symbol configuration DL %d, UL %d\n",
-                nrofDownlinkSymbols,
-                nrofUplinkSymbols);
-    AssertFatal(nb_slots_per_period == (nrofDownlinkSlots + nrofUplinkSlots + 1),
-                "set_tdd_configuration_nr: given period is inconsistent with current tdd configuration, nrofDownlinkSlots %d, "
-                "nrofUplinkSlots %d, nrofMixed slots 1, nb_slots_per_period %d \n",
-                nrofDownlinkSlots,
-                nrofUplinkSlots,
-                nb_slots_per_period);
-  }
-
-  cfg->tdd_table.max_tdd_periodicity_list = calloc(nb_slots_to_set, sizeof(nfapi_nr_max_tdd_periodicity_t));
-
+  // Allocate memory formax_num_of_symbol_per_slot_list items
+  cfg->tdd_table.max_tdd_periodicity_list = calloc_or_fail(nb_slots_to_set, sizeof(nfapi_nr_max_tdd_periodicity_t));
   for (int slot = 0; slot < nb_slots_to_set; slot++) {
     nfapi_nr_max_tdd_periodicity_t *p_list = &cfg->tdd_table.max_tdd_periodicity_list[slot];
     p_list->max_num_of_symbol_per_slot_list =
-        calloc(NR_NUMBER_OF_SYMBOLS_PER_SLOT, sizeof(*p_list->max_num_of_symbol_per_slot_list));
+        calloc_or_fail(NR_NUMBER_OF_SYMBOLS_PER_SLOT, sizeof(*p_list->max_num_of_symbol_per_slot_list));
     for (int sym = 0; sym < NR_NUMBER_OF_SYMBOLS_PER_SLOT; sym++) {
       // for each symbol, assign the TLV tag for usage when packing
       p_list->max_num_of_symbol_per_slot_list[sym].slot_config.tl.tag = NFAPI_NR_CONFIG_SLOT_CONFIG_TAG;
     }
   }
 
+  // Loop through all periods to be configured
   while (slot_number != nb_slots_to_set) {
-    if (nrofDownlinkSlots != 0) {
-      for (int sym = 0; sym < nrofDownlinkSlots * NR_NUMBER_OF_SYMBOLS_PER_SLOT; sym++) {
-        cfg->tdd_table.max_tdd_periodicity_list[slot_number]
-            .max_num_of_symbol_per_slot_list[sym % NR_NUMBER_OF_SYMBOLS_PER_SLOT]
-            .slot_config.value = 0;
-
-        if ((sym + 1) % NR_NUMBER_OF_SYMBOLS_PER_SLOT == 0)
+    slot_index = get_slot_idx_in_period(slot_number, fs);
+    /* Fill max_tdd_periodicity_list */
+    nfapi_nr_max_tdd_periodicity_t *list = &cfg->tdd_table.max_tdd_periodicity_list[slot_number];
+    // FULL DOWNLINK SLOTS
+    if (tdd_slot_bitmap[slot_index].slot_type == TDD_NR_DOWNLINK_SLOT) {
+      for (int sym = 0; sym < NR_NUMBER_OF_SYMBOLS_PER_SLOT; sym++) {
+        list->max_num_of_symbol_per_slot_list[sym % NR_NUMBER_OF_SYMBOLS_PER_SLOT].slot_config.value = 0;
+        if ((sym + 1) % NR_NUMBER_OF_SYMBOLS_PER_SLOT == 0) {
           slot_number++;
+          list += 1;
+        }
       }
     }
-
-    if (nrofDownlinkSymbols != 0 || nrofUplinkSymbols != 0) {
-      for (int sym = 0; sym < nrofDownlinkSymbols; sym++) {
-        cfg->tdd_table.max_tdd_periodicity_list[slot_number].max_num_of_symbol_per_slot_list[sym].slot_config.value = 0;
+    // FLEXIBLE SLOTS
+    if (tdd_slot_bitmap[slot_index].slot_type == TDD_NR_MIXED_SLOT) {
+      int nrofDLSymbolsInSlot = tdd_slot_bitmap[slot_index].num_dl_symbols;
+      int nrofULSymbolsInSlot = tdd_slot_bitmap[slot_index].num_ul_symbols;
+      AssertFatal(nrofDLSymbolsInSlot + nrofULSymbolsInSlot < NR_NUMBER_OF_SYMBOLS_PER_SLOT,
+                  "illegal symbol configuration DL %d, UL %d\n",
+                  nrofDLSymbolsInSlot,
+                  nrofULSymbolsInSlot);
+      // DL Symbols
+      for (int sym = 0; sym < nrofDLSymbolsInSlot; sym++) {
+        list->max_num_of_symbol_per_slot_list[sym].slot_config.value = 0;
       }
-
-      for (int sym = nrofDownlinkSymbols; sym < NR_NUMBER_OF_SYMBOLS_PER_SLOT - nrofUplinkSymbols; sym++) {
-        cfg->tdd_table.max_tdd_periodicity_list[slot_number].max_num_of_symbol_per_slot_list[sym].slot_config.value = 2;
+      // Flexible Symbols
+      for (int sym = nrofDLSymbolsInSlot; sym < NR_NUMBER_OF_SYMBOLS_PER_SLOT - nrofULSymbolsInSlot; sym++) {
+        list->max_num_of_symbol_per_slot_list[sym].slot_config.value = 2;
       }
-
-      for (int sym = NR_NUMBER_OF_SYMBOLS_PER_SLOT - nrofUplinkSymbols; sym < NR_NUMBER_OF_SYMBOLS_PER_SLOT; sym++) {
-        cfg->tdd_table.max_tdd_periodicity_list[slot_number].max_num_of_symbol_per_slot_list[sym].slot_config.value = 1;
+      // UL Symbols
+      for (int sym = NR_NUMBER_OF_SYMBOLS_PER_SLOT - nrofULSymbolsInSlot; sym < NR_NUMBER_OF_SYMBOLS_PER_SLOT; sym++) {
+        list->max_num_of_symbol_per_slot_list[sym].slot_config.value = 1;
       }
-
       slot_number++;
+      list += 1;
     }
-
-    if (nrofUplinkSlots != 0) {
-      for (int sym = 0; sym < nrofUplinkSlots * NR_NUMBER_OF_SYMBOLS_PER_SLOT; sym++) {
-        cfg->tdd_table.max_tdd_periodicity_list[slot_number]
-            .max_num_of_symbol_per_slot_list[sym % NR_NUMBER_OF_SYMBOLS_PER_SLOT]
-            .slot_config.value = 1;
-
-        if ((sym + 1) % NR_NUMBER_OF_SYMBOLS_PER_SLOT == 0)
+    // FULL UPLINK SLOTS
+    if (tdd_slot_bitmap[slot_index].slot_type == TDD_NR_UPLINK_SLOT) {
+      for (int sym = 0; sym < NR_NUMBER_OF_SYMBOLS_PER_SLOT; sym++) {
+        list->max_num_of_symbol_per_slot_list[sym % NR_NUMBER_OF_SYMBOLS_PER_SLOT].slot_config.value = 1;
+        if ((sym + 1) % NR_NUMBER_OF_SYMBOLS_PER_SLOT == 0) {
           slot_number++;
+          list += 1;
+        }
       }
     }
   }
-
-  return (nb_periods_per_frame);
+  // Dump 1 period on the output
+  for (int s = 0; s < fs->numb_slots_period; s++) {
+    if (tdd_slot_bitmap[s].slot_type == TDD_NR_MIXED_SLOT) {
+      char flexi_slot[NR_NUMBER_OF_SYMBOLS_PER_SLOT + 1];
+      memset(flexi_slot, 0, sizeof(flexi_slot));
+      for (int symb = 0; symb < NR_NUMBER_OF_SYMBOLS_PER_SLOT; symb++) {
+        uint8_t val = cfg->tdd_table.max_tdd_periodicity_list[s].max_num_of_symbol_per_slot_list[symb].slot_config.value;
+        char *symb_type_s[] = {"D", "U", "F"};
+        flexi_slot[symb] = *symb_type_s[val];
+      }
+      flexi_slot[NR_NUMBER_OF_SYMBOLS_PER_SLOT] = '\0';
+      LOG_I(NR_PHY, "TDD period configuration: slot %d is FLEXIBLE: %s\n", s, flexi_slot);
+    } else {
+      LOG_I(NR_PHY,
+            "TDD period configuration: slot %d is %s\n",
+            s,
+            tdd_slot_bitmap[s].slot_type == TDD_NR_DOWNLINK_SLOT ? "DOWNLINK" : "UPLINK");
+    }
+  }
 }
 
 /*******************************************************************
@@ -389,3 +393,48 @@ void free_tdd_configuration_dedicated_nr(NR_DL_FRAME_PARMS *frame_parms) {
   }
 }
 
+void do_tdd_config_sim(PHY_VARS_gNB *gNB, int mu)
+{
+  frame_structure_t fs = {.is_tdd = true};
+  fs.numb_slots_frame = (1 << mu) * NR_NUMBER_OF_SUBFRAMES_PER_FRAME;
+  fs.numb_period_frame = get_nb_periods_per_frame(gNB->gNB_config.tdd_table.tdd_period.value);
+  fs.numb_slots_period = fs.numb_slots_frame / fs.numb_period_frame;
+  tdd_period_config_t *pc = &fs.period_cfg;
+  // Set DL/UL slots
+  switch (mu)
+  {
+  case 0:
+    pc->num_dl_slots = 3;
+    pc->num_ul_slots = 1;
+    break;
+  
+  case 1:
+    pc->num_dl_slots = 7;
+    pc->num_ul_slots = 2;
+    break;
+
+  case 3:
+    pc->num_dl_slots = 27;
+    pc->num_ul_slots = 12;
+    break;
+
+  default:
+    break;
+  }
+  for (int i = 0; i < pc->num_dl_slots; i++) {
+    pc->tdd_slot_bitmap[i].slot_type = TDD_NR_DOWNLINK_SLOT;
+    pc->tdd_slot_bitmap[i].num_dl_symbols = 0;
+    pc->tdd_slot_bitmap[i].num_ul_symbols = 0;
+  }
+  for (int i = pc->num_dl_slots; i < fs.numb_slots_period - pc->num_ul_slots; i++) {
+    pc->tdd_slot_bitmap[i].slot_type = TDD_NR_MIXED_SLOT;
+    pc->tdd_slot_bitmap[i].num_dl_symbols = 6;
+    pc->tdd_slot_bitmap[i].num_ul_symbols = 4;
+  }
+  for (int i = pc->num_ul_slots; i < fs.numb_slots_period; i++) {
+    pc->tdd_slot_bitmap[i].slot_type = TDD_NR_UPLINK_SLOT;
+    pc->tdd_slot_bitmap[i].num_dl_symbols = 0;
+    pc->tdd_slot_bitmap[i].num_ul_symbols = 0;
+  }
+  set_tdd_config_nr(&gNB->gNB_config, &fs);
+}