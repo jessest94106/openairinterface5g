jessest94106/openairinterface5g

commit 072e3070220de9a26945a18146686c86d9de7a45

set correct parameter type, and fuse funcions that do same real/complex vector operations, doing this, fix a lot of unitary multiplications (else there would be numerous cast operations

openair1/PHY/NR_TRANSPORT/nr_ulsch_demodulation.c
@@ -308,14 +308,14 @@ static void nr_ulsch_channel_compensation(c16_t *rxFext,
 }
 
 // Zero Forcing Rx function: nr_det_HhH()
-static void nr_ulsch_det_HhH (int32_t *after_mf_00,//a
-                              int32_t *after_mf_01,//b
-                              int32_t *after_mf_10,//c
-                              int32_t *after_mf_11,//d
-                              uint32_t *det_fin,//1/ad-bc
-                              unsigned short nb_rb,
-                              unsigned char symbol,
-                              int32_t shift)
+static void nr_ulsch_det_HhH(c16_t *after_mf_00, // a
+                             c16_t *after_mf_01, // b
+                             c16_t *after_mf_10, // c
+                             c16_t *after_mf_11, // d
+                             uint32_t *det_fin, // 1/ad-bc
+                             unsigned short nb_rb,
+                             unsigned char symbol,
+                             int32_t shift)
 {
   simde__m128i *after_mf_00_128,*after_mf_01_128, *after_mf_10_128, *after_mf_11_128, ad_re_128, bc_re_128; //ad_im_128, bc_im_128;
   simde__m128i *det_fin_128, det_re_128; //det_im_128, tmp_det0, tmp_det1;
@@ -365,14 +365,10 @@ static void nr_ulsch_det_HhH (int32_t *after_mf_00,//a
  *
  * */
 // TODO: This function is just a wrapper, can be removed.
-static void nr_ulsch_conjch0_mult_ch1(int *ch0,
-                                      int *ch1,
-                                      int32_t *ch0conj_ch1,
-                                      unsigned short nb_rb,
-                                      unsigned char output_shift0)
+static void nr_ulsch_conjch0_mult_ch1(c16_t *ch0, c16_t *ch1, c16_t *ch0conj_ch1, unsigned short nb_rb, unsigned char output_shift0)
 {
   //This function is used to compute multiplications in H_hermitian * H matrix
-  mult_cpx_conj_vector((c16_t *)ch0, (c16_t *)ch1, (c16_t *)ch0conj_ch1, 12 * nb_rb, output_shift0);
+  mult_cpx_conj_vector(ch0, ch1, ch0conj_ch1, 12 * nb_rb, output_shift0);
 }
 
 static simde__m128i nr_ulsch_comp_muli_sum(simde__m128i input_x,
@@ -422,26 +418,26 @@ static simde__m128i nr_ulsch_comp_muli_sum(simde__m128i input_x,
  *
  *
  * */
-static void nr_ulsch_construct_HhH_elements(int *conjch00_ch00,
-                                            int *conjch01_ch01,
-                                            int *conjch11_ch11,
-                                            int *conjch10_ch10,//
-                                            int *conjch20_ch20,
-                                            int *conjch21_ch21,
-                                            int *conjch30_ch30,
-                                            int *conjch31_ch31,
-                                            int *conjch00_ch01,//00_01
-                                            int *conjch01_ch00,//01_00
-                                            int *conjch10_ch11,//10_11
-                                            int *conjch11_ch10,//11_10
-                                            int *conjch20_ch21,
-                                            int *conjch21_ch20,
-                                            int *conjch30_ch31,
-                                            int *conjch31_ch30,
-                                            int32_t *after_mf_00,
-                                            int32_t *after_mf_01,
-                                            int32_t *after_mf_10,
-                                            int32_t *after_mf_11,
+static void nr_ulsch_construct_HhH_elements(c16_t *conjch00_ch00,
+                                            c16_t *conjch01_ch01,
+                                            c16_t *conjch11_ch11,
+                                            c16_t *conjch10_ch10, //
+                                            c16_t *conjch20_ch20,
+                                            c16_t *conjch21_ch21,
+                                            c16_t *conjch30_ch30,
+                                            c16_t *conjch31_ch31,
+                                            c16_t *conjch00_ch01, // 00_01
+                                            c16_t *conjch01_ch00, // 01_00
+                                            c16_t *conjch10_ch11, // 10_11
+                                            c16_t *conjch11_ch10, // 11_10
+                                            c16_t *conjch20_ch21,
+                                            c16_t *conjch21_ch20,
+                                            c16_t *conjch30_ch31,
+                                            c16_t *conjch31_ch30,
+                                            c16_t *after_mf_00,
+                                            c16_t *after_mf_01,
+                                            c16_t *after_mf_10,
+                                            c16_t *after_mf_11,
                                             unsigned short nb_rb,
                                             unsigned char symbol)
 {
@@ -532,52 +528,54 @@ static uint8_t nr_ulsch_mmse_2layers(NR_DL_FRAME_PARMS *frame_parms,
                                      int **ul_ch_mag,
                                      int **ul_ch_magb,
                                      int **ul_ch_magc,
-                                     int **ul_ch_estimates_ext,
+                                     int nb_layer,
+                                     int nb_rx_ant,
+                                     uint32_t buffer_length,
+                                     c16_t ul_ch_estimates_ext[nb_layer][nb_rx_ant][buffer_length],
                                      unsigned short nb_rb,
                                      unsigned char n_rx,
                                      unsigned char mod_order,
                                      int shift,
                                      unsigned char symbol,
                                      int length,
-                                     uint32_t noise_var,
-                                     uint32_t buffer_length)
+                                     uint32_t noise_var)
 {
-  int *ch00, *ch01, *ch10, *ch11;
-  int *ch20, *ch30, *ch21, *ch31;
+  c16_t *ch00, *ch01, *ch10, *ch11;
+  c16_t *ch20, *ch30, *ch21, *ch31;
   uint32_t nb_rb_0 = length/12 + ((length%12)?1:0);
 
   /* we need at least alignment to 16 bytes, let's put 32 to be sure
    * (maybe not necessary but doesn't hurt)
    */
-  int32_t conjch00_ch01[12*nb_rb] __attribute__((aligned(32)));
-  int32_t conjch01_ch00[12*nb_rb] __attribute__((aligned(32)));
-  int32_t conjch10_ch11[12*nb_rb] __attribute__((aligned(32)));
-  int32_t conjch11_ch10[12*nb_rb] __attribute__((aligned(32)));
-  int32_t conjch00_ch00[12*nb_rb] __attribute__((aligned(32)));
-  int32_t conjch01_ch01[12*nb_rb] __attribute__((aligned(32)));
-  int32_t conjch10_ch10[12*nb_rb] __attribute__((aligned(32)));
-  int32_t conjch11_ch11[12*nb_rb] __attribute__((aligned(32)));
-  int32_t conjch20_ch20[12*nb_rb] __attribute__((aligned(32)));
-  int32_t conjch21_ch21[12*nb_rb] __attribute__((aligned(32)));
-  int32_t conjch30_ch30[12*nb_rb] __attribute__((aligned(32)));
-  int32_t conjch31_ch31[12*nb_rb] __attribute__((aligned(32)));
-  int32_t conjch20_ch21[12*nb_rb] __attribute__((aligned(32)));
-  int32_t conjch30_ch31[12*nb_rb] __attribute__((aligned(32)));
-  int32_t conjch21_ch20[12*nb_rb] __attribute__((aligned(32)));
-  int32_t conjch31_ch30[12*nb_rb] __attribute__((aligned(32)));
+  c16_t conjch00_ch01[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t conjch01_ch00[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t conjch10_ch11[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t conjch11_ch10[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t conjch00_ch00[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t conjch01_ch01[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t conjch10_ch10[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t conjch11_ch11[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t conjch20_ch20[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t conjch21_ch21[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t conjch30_ch30[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t conjch31_ch31[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t conjch20_ch21[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t conjch30_ch31[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t conjch21_ch20[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t conjch31_ch30[12 * nb_rb] __attribute__((aligned(32)));
 
-  int32_t af_mf_00[12*nb_rb] __attribute__((aligned(32)));
-  int32_t af_mf_01[12*nb_rb] __attribute__((aligned(32)));
-  int32_t af_mf_10[12*nb_rb] __attribute__((aligned(32)));
-  int32_t af_mf_11[12*nb_rb] __attribute__((aligned(32)));
+  c16_t af_mf_00[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t af_mf_01[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t af_mf_10[12 * nb_rb] __attribute__((aligned(32)));
+  c16_t af_mf_11[12 * nb_rb] __attribute__((aligned(32)));
   uint32_t determ_fin[12*nb_rb] __attribute__((aligned(32)));
 
   switch (n_rx) {
     case 2://
-      ch00 = &((int *)ul_ch_estimates_ext)[0 * buffer_length];
-      ch01 = &((int *)ul_ch_estimates_ext)[2 * buffer_length];
-      ch10 = &((int *)ul_ch_estimates_ext)[1 * buffer_length];
-      ch11 = &((int *)ul_ch_estimates_ext)[3 * buffer_length];
+      ch00 = ul_ch_estimates_ext[0][0];
+      ch01 = ul_ch_estimates_ext[1][0];
+      ch10 = ul_ch_estimates_ext[0][1];
+      ch11 = ul_ch_estimates_ext[1][1];
       ch20 = NULL;
       ch21 = NULL;
       ch30 = NULL;
@@ -585,14 +583,14 @@ static uint8_t nr_ulsch_mmse_2layers(NR_DL_FRAME_PARMS *frame_parms,
       break;
 
     case 4://
-      ch00 = &((int *)ul_ch_estimates_ext)[0 * buffer_length];
-      ch01 = &((int *)ul_ch_estimates_ext)[4 * buffer_length];
-      ch10 = &((int *)ul_ch_estimates_ext)[1 * buffer_length];
-      ch11 = &((int *)ul_ch_estimates_ext)[5 * buffer_length];
-      ch20 = &((int *)ul_ch_estimates_ext)[2 * buffer_length];
-      ch21 = &((int *)ul_ch_estimates_ext)[6 * buffer_length];
-      ch30 = &((int *)ul_ch_estimates_ext)[3 * buffer_length];
-      ch31 = &((int *)ul_ch_estimates_ext)[7 * buffer_length];
+      ch00 = ul_ch_estimates_ext[0][0];
+      ch01 = ul_ch_estimates_ext[1][0];
+      ch10 = ul_ch_estimates_ext[0][1];
+      ch11 = ul_ch_estimates_ext[1][1];
+      ch20 = ul_ch_estimates_ext[2][0];
+      ch21 = ul_ch_estimates_ext[2][1];
+      ch30 = ul_ch_estimates_ext[3][0];
+      ch31 = ul_ch_estimates_ext[3][1];
       break;
 
     default:
@@ -1031,15 +1029,17 @@ static void inner_rx(PHY_VARS_gNB *gNB,
                             (int **)rxF_ch_maga,
                             (int **)rxF_ch_magb,
                             (int **)rxF_ch_magc,
-                            (int **)chFext,
+                            nb_layer,
+                            nb_rx_ant,
+                            buffer_length,
+                            chFext,
                             rel15_ul->rb_size,
                             frame_parms->nb_antennas_rx,
                             rel15_ul->qam_mod_order,
                             pusch_vars->log2_maxh,
                             symbol,
                             pusch_vars->ul_valid_re_per_slot[symbol],
-                            nvar,
-                            buffer_length);
+                            nvar);
     }
   }
   if (nb_layer != 2 || rel15_ul->qam_mod_order > 6)

openair1/PHY/LTE_ESTIMATION/freq_equalization.c
@@ -287,9 +287,9 @@ void init_fde() {
 }
 
 void freq_equalization(LTE_DL_FRAME_PARMS *frame_parms,
-                       int32_t **rxdataF_comp,
-                       int32_t **ul_ch_mag,
-                       int32_t **ul_ch_magb,
+                       c16_t **rxdataF_comp,
+                       c16_t **ul_ch_mag,
+                       c16_t **ul_ch_magb,
                        uint8_t symbol,
                        uint16_t Msc_RS,
                        uint8_t Qm)

openair1/PHY/LTE_ESTIMATION/lte_dl_mbsfn_channel_estimation.c
@@ -46,7 +46,7 @@ int lte_dl_mbsfn_channel_estimation(PHY_VARS_UE *ue,
   int pilot[600] __attribute__((aligned(16)));
   unsigned char aarx,aa;
   unsigned int rb;
-  short *pil,*rxF,*ch,*ch0,*ch1,*ch11,*chp,*ch_prev;
+  short *pil, *rxF, *ch, *ch0, *ch1, *ch11, *chp;
   int ch_offset,symbol_offset;
   //  unsigned int n;
   //  int i;
@@ -684,37 +684,37 @@ int lte_dl_mbsfn_channel_estimation(PHY_VARS_UE *ue,
     if (l==6) {
       ch = (short *)&dl_ch_estimates[aarx][ch_offset];
       //        printf("Interpolating ch 2,6 => %d\n",ch_offset);
-      ch_prev = (short *)&dl_ch_estimates[aarx][2 * (symb_sz)];
+      c16_t *ch_prev = (c16_t *)&dl_ch_estimates[aarx][2 * symb_sz];
       ch0 = (short *)&dl_ch_estimates[aarx][0 * (symb_sz)];
       memcpy(ch0, ch_prev, 4 * symb_sz);
       ch1 = (short *)&dl_ch_estimates[aarx][1 * (symb_sz)];
       memcpy(ch1, ch_prev, 4 * symb_sz);
       // 3/4 ch2 + 1/4 ch6 => ch3
-      mult_complex_vector_real_scalar(ch_prev, 24576, ch_prev + (2 * (symb_sz)), symb_sz);
-      multadd_complex_vector_real_scalar(ch, 8192, ch_prev + (2 * (symb_sz)), symb_sz);
+      mult_complex_vector_real_scalar(ch_prev, 24576, ch_prev + symb_sz, symb_sz);
+      multadd_complex_vector_real_scalar((c16_t *)ch, 8192, ch_prev + symb_sz, symb_sz);
       // 1/2 ch2 + 1/2 ch6 => ch4
-      mult_complex_vector_real_scalar(ch_prev, 16384, ch_prev + (4 * (symb_sz)), symb_sz);
-      multadd_complex_vector_real_scalar(ch, 16384, ch_prev + (4 * (symb_sz)), symb_sz);
+      mult_complex_vector_real_scalar(ch_prev, 16384, ch_prev + 2 * symb_sz, symb_sz);
+      multadd_complex_vector_real_scalar((c16_t *)ch, 16384, ch_prev + 2 * symb_sz, symb_sz);
       // 1/4 ch2 + 3/4 ch6 => ch5
-      mult_complex_vector_real_scalar(ch_prev, 8192, ch_prev + (6 * ((symb_sz))), symb_sz);
-      multadd_complex_vector_real_scalar(ch, 24576, ch_prev + (6 * ((symb_sz))), symb_sz);
+      mult_complex_vector_real_scalar(ch_prev, 8192, ch_prev + 3 * symb_sz, symb_sz);
+      multadd_complex_vector_real_scalar((c16_t *)ch, 24576, ch_prev + 3 * symb_sz, symb_sz);
     }
 
     if (l==10) {
       ch = (short *)&dl_ch_estimates[aarx][ch_offset];
-      ch_prev = (short *)&dl_ch_estimates[aarx][6 * (symb_sz)];
+      c16_t *ch_prev = (c16_t *)&dl_ch_estimates[aarx][6 * symb_sz];
       // 3/4 ch6 + 1/4 ch10 => ch7
-      mult_complex_vector_real_scalar(ch_prev, 24576, ch_prev + (2 * (symb_sz)), symb_sz);
-      multadd_complex_vector_real_scalar(ch, 8192, ch_prev + (2 * (symb_sz)), symb_sz);
+      mult_complex_vector_real_scalar(ch_prev, 24576, ch_prev + symb_sz, symb_sz);
+      multadd_complex_vector_real_scalar((c16_t *)ch, 8192, ch_prev + symb_sz, symb_sz);
       // 1/2 ch6 + 1/2 ch10 => ch8
-      mult_complex_vector_real_scalar(ch_prev, 16384, ch_prev + (4 * (symb_sz)), symb_sz);
-      multadd_complex_vector_real_scalar(ch, 16384, ch_prev + (4 * (symb_sz)), symb_sz);
+      mult_complex_vector_real_scalar(ch_prev, 16384, ch_prev + 2 * symb_sz, symb_sz);
+      multadd_complex_vector_real_scalar((c16_t *)ch, 16384, ch_prev + 2 * symb_sz, symb_sz);
       // 1/4 ch6 + 3/4 ch10 => ch9
-      mult_complex_vector_real_scalar(ch_prev, 8192, ch_prev + (6 * ((symb_sz))), symb_sz);
-      multadd_complex_vector_real_scalar(ch, 24576, ch_prev + (6 * ((symb_sz))), symb_sz);
+      mult_complex_vector_real_scalar(ch_prev, 8192, ch_prev + 3 * symb_sz, symb_sz);
+      multadd_complex_vector_real_scalar((c16_t *)ch, 24576, ch_prev + 3 * symb_sz, symb_sz);
       // 5/4 ch10 - 1/4 ch6 => ch11
       // Ch11
-      ch_prev = (short *)&dl_ch_estimates[aarx][10 * (symb_sz)];
+      ch_prev = (c16_t *)&dl_ch_estimates[aarx][10 * (symb_sz)];
       ch11 = (short *)&dl_ch_estimates[aarx][11 * (symb_sz)];
       memcpy(ch11, ch_prev, 4 * symb_sz);
     }
@@ -764,13 +764,10 @@ int lte_dl_mbsfn_khz_1dot25_channel_estimation(PHY_VARS_UE *ue,
     int subframe) {
   int pilot_khz_1dot25[2*2*600] __attribute__((aligned(16)));
   unsigned char aarx,aa;
-  //unsigned int rb;
-  int16_t ch[2];
-  short *pil,*rxF,*dl_ch/*,*ch0,*ch1,*ch11,*chp,*ch_prev*/;
+  // unsigned int rb;
   int ch_offset,symbol_offset;
   int pilot_cnt;
-  int16_t *f,*f2,*fl,*f2l2,*fr,*f2r2/*,*f2_dc,*f_dc*/;
-  unsigned int k;
+  int16_t *f, *f2, *fl, *f2l2, *fr, *f2r2 /*,*f2_dc,*f_dc*/;
   //  unsigned int n;
   //  int i;
   int **dl_ch_estimates=ue->common_vars.common_vars_rx_data_per_thread[ue->current_thread_id[subframe]].dl_ch_estimates[0];
@@ -801,101 +798,46 @@ int lte_dl_mbsfn_khz_1dot25_channel_estimation(PHY_VARS_UE *ue,
 
   for (aarx=0; aarx<ue->frame_parms.nb_antennas_rx; aarx++) {
     // generate pilot
-    lte_dl_mbsfn_khz_1dot25_rx(ue,
-                               &pilot_khz_1dot25[0],
-                               subframe);
-    pil   = (short *)&pilot_khz_1dot25[0];
-    rxF   = (short *)&rxdataF[aarx][((ue->frame_parms.first_carrier_offset_khz_1dot25))];
-    dl_ch = (short *)&dl_ch_estimates[aarx][ch_offset];
-    memset(dl_ch,0,4*(ue->frame_parms.ofdm_symbol_size_khz_1dot25));
+    lte_dl_mbsfn_khz_1dot25_rx(ue, pilot_khz_1dot25, subframe);
+    c16_t *pil = (c16_t *)pilot_khz_1dot25;
+    c16_t *dl_ch = (c16_t *)&dl_ch_estimates[aarx][ch_offset];
+    memset(dl_ch, 0, sizeof(c16_t) * ue->frame_parms.ofdm_symbol_size_khz_1dot25);
 
-    if( (subframe&0x1) == 0) {
-      rxF+=0;
-      k=0;
-    } else {
-      rxF+=6;//2*3;
-      k=3;
-    }
+    unsigned int k = (subframe & 0x1) == 0 ? 0 : 3;
+    c16_t *rxF = (c16_t *)&rxdataF[aarx][ue->frame_parms.first_carrier_offset_khz_1dot25 + k];
 
-    if(1/*ue->frame_parms.N_RB_DL==25*/) {
-      ch[0] = (int16_t)(((int32_t)pil[0]*rxF[0] - (int32_t)pil[1]*rxF[1])>>15);
-      ch[1] = (int16_t)(((int32_t)pil[0]*rxF[1] + (int32_t)pil[1]*rxF[0])>>15);
-      multadd_real_vector_complex_scalar(fl,
-                                         ch,
-                                         dl_ch,
-                                         24);
-      pil+=2;    // Re Im
-      rxF+=12;
-      dl_ch+=8;
-      ch[0] = (int16_t)(((int32_t)pil[0]*rxF[0] - (int32_t)pil[1]*rxF[1])>>15);
-      ch[1] = (int16_t)(((int32_t)pil[0]*rxF[1] + (int32_t)pil[1]*rxF[0])>>15);
-      multadd_real_vector_complex_scalar(f2l2,
-                                         ch,
-                                         dl_ch,
-                                         24);
-      pil+=2;    // Re Im
-      rxF+=12;
-      dl_ch+=16;
+    if (1 /*ue->frame_parms.N_RB_DL==25*/) {
+      multadd_real_vector_complex_scalar(fl, c16mulShift(*pil++, *rxF, 15), dl_ch, 24);
+      rxF += 6;
+      dl_ch += 4;
+      multadd_real_vector_complex_scalar(f2l2, c16mulShift(*pil++, *rxF, 15), dl_ch, 24);
+      rxF += 6;
+      dl_ch += 8;
 
-      for(pilot_cnt=2; pilot_cnt<ue->frame_parms.N_RB_DL*12-1/*299*/; pilot_cnt+=2) {
-        ch[0] = (int16_t)(((int32_t)pil[0]*rxF[0] - (int32_t)pil[1]*rxF[1])>>15);
-        ch[1] = (int16_t)(((int32_t)pil[0]*rxF[1] + (int32_t)pil[1]*rxF[0])>>15);
-        multadd_real_vector_complex_scalar(f,
-                                           ch,
-                                           dl_ch,
-                                           24);
-        pil+=2;    // Re Im
-        rxF+=12;
-        dl_ch+=8;
-        ch[0] = (int16_t)(((int32_t)pil[0]*rxF[0] - (int32_t)pil[1]*rxF[1])>>15);
-        ch[1] = (int16_t)(((int32_t)pil[0]*rxF[1] + (int32_t)pil[1]*rxF[0])>>15);
-        multadd_real_vector_complex_scalar(f2,
-                                           ch,
-                                           dl_ch,
-                                           24);
-        pil+=2;
-        rxF+=12;
-        dl_ch+=16;
+      for (pilot_cnt = 2; pilot_cnt < ue->frame_parms.N_RB_DL * 12 - 1 /*299*/; pilot_cnt += 2) {
+        multadd_real_vector_complex_scalar(f, c16mulShift(*pil++, *rxF, 15), dl_ch, 24);
+        rxF += 6;
+        dl_ch += 4;
+        multadd_real_vector_complex_scalar(f2, c16mulShift(*pil++, *rxF, 15), dl_ch, 24);
+        rxF += 6;
+        dl_ch += 8;
       }
 
-      rxF   = (int16_t *)&rxdataF[aarx][((symbol_offset+1+k))]; //Skip DC offset
+      rxF = (c16_t *)&rxdataF[aarx][symbol_offset + 1 + k]; // Skip DC offset
 
-      for(pilot_cnt=0; pilot_cnt<ue->frame_parms.N_RB_DL*12-3/*297*/; pilot_cnt+=2) {
-        ch[0] = (int16_t)(((int32_t)pil[0]*rxF[0] - (int32_t)pil[1]*rxF[1])>>15);
-        ch[1] = (int16_t)(((int32_t)pil[0]*rxF[1] + (int32_t)pil[1]*rxF[0])>>15);
-        multadd_real_vector_complex_scalar(f,
-                                           ch,
-                                           dl_ch,
-                                           24);
-        pil+=2;
-        rxF+=12;
-        dl_ch+=8;
-        ch[0] = (int16_t)(((int32_t)pil[0]*rxF[0] - (int32_t)pil[1]*rxF[1])>>15);
-        ch[1] = (int16_t)(((int32_t)pil[0]*rxF[1] + (int32_t)pil[1]*rxF[0])>>15);
-        multadd_real_vector_complex_scalar(f2,
-                                           ch,
-                                           dl_ch,
-                                           24);
-        pil+=2;
-        rxF+=12;
-        dl_ch+=16;
+      for (pilot_cnt = 0; pilot_cnt < ue->frame_parms.N_RB_DL * 12 - 3 /*297*/; pilot_cnt += 2) {
+        multadd_real_vector_complex_scalar(f, c16mulShift(*pil++, *rxF, 15), dl_ch, 24);
+        rxF += 6;
+        dl_ch += 4;
+        multadd_real_vector_complex_scalar(f2, c16mulShift(*pil++, *rxF, 15), dl_ch, 24);
+        rxF += 6;
+        dl_ch += 8;
       }
 
-      ch[0] = (int16_t)(((int32_t)pil[0]*rxF[0] - (int32_t)pil[1]*rxF[1])>>15);
-      ch[1] = (int16_t)(((int32_t)pil[0]*rxF[1] + (int32_t)pil[1]*rxF[0])>>15);
-      multadd_real_vector_complex_scalar(fr,
-                                         ch,
-                                         dl_ch,
-                                         24);
-      pil+=2;    // Re Im
-      rxF+=12;
-      dl_ch+=8;
-      ch[0] = (int16_t)(((int32_t)pil[0]*rxF[0] - (int32_t)pil[1]*rxF[1])>>15);
-      ch[1] = (int16_t)(((int32_t)pil[0]*rxF[1] + (int32_t)pil[1]*rxF[0])>>15);
-      multadd_real_vector_complex_scalar(f2r2,
-                                         ch,
-                                         dl_ch,
-                                         24);
+      multadd_real_vector_complex_scalar(fr, c16mulShift(*pil++, *rxF, 15), dl_ch, 24);
+      rxF += 6;
+      dl_ch += 4;
+      multadd_real_vector_complex_scalar(f2r2, c16mulShift(*pil, *rxF, 15), dl_ch, 24);
     }
   }
 

openair1/PHY/LTE_ESTIMATION/lte_estimation.h
@@ -53,9 +53,7 @@ The algorithm uses a time domain correlation with a downsampled version of the r
 \param eNB_id return value with the eNb_id
 \return sync_pos Position of the sync within the frame (downsampled) if successfull and -1 if there was an error or no peak was detected.
  */
-int lte_sync_time(int **rxdata,
-                  LTE_DL_FRAME_PARMS *frame_parms,
-                  int *eNB_id);
+int lte_sync_time(c16_t **rxdata, LTE_DL_FRAME_PARMS *frame_parms, int *eNB_id);
 
 /*!
 \brief This function performs the coarse frequency and PSS synchronization.
@@ -194,19 +192,19 @@ void phy_adjust_gain (PHY_VARS_UE *phy_vars_ue,
                       unsigned char eNB_id);
 
 int32_t lte_ul_channel_estimation(LTE_DL_FRAME_PARMS *frame_parms,
-                              L1_rxtx_proc_t *proc,
-				  LTE_eNB_ULSCH_t * ulsch,
-				  int32_t **ul_ch_estimates,
-				  int32_t **ul_ch_estimates_time,
-				  int32_t **rxdataF_ext,
-                              module_id_t UE_id,
-                              uint8_t l,
-                              uint8_t Ns);
+                                  L1_rxtx_proc_t *proc,
+                                  LTE_eNB_ULSCH_t *ulsch,
+                                  c16_t **ul_ch_estimates,
+                                  c16_t **ul_ch_estimates_time,
+                                  c16_t **rxdataF_ext,
+                                  module_id_t UE_id,
+                                  uint8_t l,
+                                  uint8_t Ns);
 
 int32_t lte_ul_channel_estimation_RRU(LTE_DL_FRAME_PARMS *frame_parms,
-                                      int32_t **ul_ch_estimates,
-                                      int32_t **ul_ch_estimates_time,
-                                      int32_t **rxdataF_ext,
+                                      c16_t **ul_ch_estimates,
+                                      c16_t **ul_ch_estimates_time,
+                                      c16_t **rxdataF_ext,
                                       int N_rb_alloc,
                                       int frame_rx,
                                       int subframe_rx,
@@ -217,9 +215,7 @@ int32_t lte_ul_channel_estimation_RRU(LTE_DL_FRAME_PARMS *frame_parms,
                                       int interpolate,
                                       uint16_t rnti);
 
-int16_t lte_ul_freq_offset_estimation(LTE_DL_FRAME_PARMS *frame_parms,
-                                      int32_t *ul_ch_estimates,
-                                      uint16_t nb_rb);
+int16_t lte_ul_freq_offset_estimation(LTE_DL_FRAME_PARMS *frame_parms, c16_t *ul_ch_estimates, uint16_t nb_rb);
 
 int lte_srs_channel_estimation(LTE_DL_FRAME_PARMS *frame_parms,
                                LTE_eNB_COMMON *eNB_common_vars,
@@ -235,8 +231,7 @@ int lte_est_timing_advance(LTE_DL_FRAME_PARMS *frame_parms,
                            unsigned char number_of_cards,
                            short coef);
 
-int lte_est_timing_advance_pusch(LTE_DL_FRAME_PARMS *frame_parms,
-                                 int32_t **ul_ch_estimates_time);
+int lte_est_timing_advance_pusch(LTE_DL_FRAME_PARMS *frame_parms, c16_t **ul_ch_estimates_time);
 
 void lte_eNB_I0_measurements(PHY_VARS_eNB *phy_vars_eNB,
                              int subframe,
@@ -252,11 +247,10 @@ void lte_eNB_srs_measurements(PHY_VARS_eNB *phy_vars_eNBy,
                               module_id_t UE_id,
                               unsigned char init_averaging);
 
-
 void freq_equalization(LTE_DL_FRAME_PARMS *frame_parms,
-                       int **rxdataF_comp,
-                       int **ul_ch_mag,
-                       int **ul_ch_mag_b,
+                       c16_t **rxdataF_comp,
+                       c16_t **ul_ch_mag,
+                       c16_t **ul_ch_mag_b,
                        unsigned char symbol,
                        unsigned short Msc_RS,
                        unsigned char Qm);

openair1/PHY/LTE_ESTIMATION/lte_sync_time.c
@@ -114,7 +114,7 @@ void lte_sync_time_free(void) {
 
 #define SHIFT 17
 
-int lte_sync_time(int **rxdata, LTE_DL_FRAME_PARMS *frame_parms, int *eNB_id)
+int lte_sync_time(c16_t **rxdata, LTE_DL_FRAME_PARMS *frame_parms, int *eNB_id)
 {
   // perform a time domain correlation using the oversampled sync sequence
   unsigned int n, ar, s, peak_pos, peak_val, sync_source;
@@ -135,8 +135,8 @@ int lte_sync_time(int **rxdata, LTE_DL_FRAME_PARMS *frame_parms, int *eNB_id)
       //calculate dot product of primary_synch0_time and rxdata[ar][n] (ar=0..nb_ant_rx) and store the sum in temp[n];
       for (int i = 0; i < sizeofArray(primary_synch_time); i++)
         for (ar = 0; ar < frame_parms->nb_antennas_rx; ar++) {
-          c32_t result = dot_product(primary_synch_time[i], (c16_t *)&(rxdata[ar][n]), frame_parms->ofdm_symbol_size, SHIFT);
-          c32_t result2 = dot_product(primary_synch_time[i], (c16_t *)&(rxdata[ar][n + length]), frame_parms->ofdm_symbol_size, SHIFT);
+          c32_t result = dot_product(primary_synch_time[i], rxdata[ar] + n, frame_parms->ofdm_symbol_size, SHIFT);
+          c32_t result2 = dot_product(primary_synch_time[i], rxdata[ar] + n + length, frame_parms->ofdm_symbol_size, SHIFT);
           sync_out[i].r += result.r;
           sync_out[i].i += result.i;
           sync_out2[i].r += result2.r;
@@ -228,9 +228,7 @@ int ru_sync_time(RU_t *ru,
 
   // circular copy of beginning to end of rxdata buffer. Note: buffer should be big enough upon calling this function
   for (int ar=0; ar<ru->nb_rx; ar++)
-    memcpy((void *)&ru->common.rxdata[ar][2*length],
-           (void *)&ru->common.rxdata[ar][0],
-           frame_parms->ofdm_symbol_size);
+    memcpy(ru->common.rxdata[ar] + 2 * length, ru->common.rxdata[ar], frame_parms->ofdm_symbol_size);
 
   int32_t maxlev0=0;
   int     maxpos0=0;

commit 06e7ba0f0b9776c3c92e035f4c290d3eeede1ed7

Fix UL chanel estimates mapping in 2-layer MMSE receiver
Resolve issue introduced in 072e3070220de9a26945a18146686c86d9de7a45.

openair1/PHY/NR_TRANSPORT/nr_ulsch_demodulation.c
@@ -587,10 +587,10 @@ static uint8_t nr_ulsch_mmse_2layers(NR_DL_FRAME_PARMS *frame_parms,
       ch01 = ul_ch_estimates_ext[1][0];
       ch10 = ul_ch_estimates_ext[0][1];
       ch11 = ul_ch_estimates_ext[1][1];
-      ch20 = ul_ch_estimates_ext[2][0];
-      ch21 = ul_ch_estimates_ext[2][1];
-      ch30 = ul_ch_estimates_ext[3][0];
-      ch31 = ul_ch_estimates_ext[3][1];
+      ch20 = ul_ch_estimates_ext[0][2];
+      ch21 = ul_ch_estimates_ext[1][2];
+      ch30 = ul_ch_estimates_ext[0][3];
+      ch31 = ul_ch_estimates_ext[1][3];
       break;
 
     default: