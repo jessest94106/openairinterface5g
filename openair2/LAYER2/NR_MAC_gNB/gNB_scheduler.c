jessest94106/openairinterface5g

commit 408750864eae8980d77cefec66e5bae2771881b8

print stats only after scheduler started

openair2/LAYER2/NR_MAC_gNB/gNB_scheduler.c
@@ -194,7 +194,8 @@ void gNB_dlsch_ulsch_scheduler(module_id_t module_idP, frame_t frame, slot_t slo
     clear_nr_nfapi_information(gNB, CC_id, frame, slot, &sched_info->DL_req, &sched_info->TX_req, &sched_info->UL_dci_req);
   }
 
-  if ((slot == 0) && (frame & 127) == 0) {
+  bool wait_prach_completed = gNB->num_scheduled_prach_rx >= NUM_PRACH_RX_FOR_NOISE_ESTIMATE;
+  if (wait_prach_completed && (slot == 0) && (frame & 127) == 0) {
     char stats_output[32656] = {0};
     dump_mac_stats(gNB, stats_output, sizeof(stats_output), true);
     LOG_I(NR_MAC, "Frame.Slot %d.%d\n%s\n", frame, slot, stats_output);
@@ -202,7 +203,7 @@ void gNB_dlsch_ulsch_scheduler(module_id_t module_idP, frame_t frame, slot_t slo
 
   nr_mac_update_timers(module_idP, frame, slot);
 
-  if (gNB->num_scheduled_prach_rx >= NUM_PRACH_RX_FOR_NOISE_ESTIMATE || get_softmodem_params()->phy_test) {
+  if (wait_prach_completed || get_softmodem_params()->phy_test) {
     // This schedules MIB
     schedule_nr_mib(module_idP, frame, slot, &sched_info->DL_req);
 

commit 1234ffa4ddf4eb7291dbb6e74376c7ddd71962c7

Print gNB MAC stats in phy-test mode as well
There is no PRACH, so the condition to print stats would never be
reached.

Fixes: 408750864eae ("print stats only after scheduler started")

openair2/LAYER2/NR_MAC_gNB/gNB_scheduler.c
@@ -195,7 +195,7 @@ void gNB_dlsch_ulsch_scheduler(module_id_t module_idP, frame_t frame, slot_t slo
   }
 
   bool wait_prach_completed = gNB->num_scheduled_prach_rx >= NUM_PRACH_RX_FOR_NOISE_ESTIMATE;
-  if (wait_prach_completed && (slot == 0) && (frame & 127) == 0) {
+  if ((wait_prach_completed || get_softmodem_params()->phy_test) && (slot == 0) && (frame & 127) == 0) {
     char stats_output[32656] = {0};
     dump_mac_stats(gNB, stats_output, sizeof(stats_output), true);
     LOG_I(NR_MAC, "Frame.Slot %d.%d\n%s\n", frame, slot, stats_output);