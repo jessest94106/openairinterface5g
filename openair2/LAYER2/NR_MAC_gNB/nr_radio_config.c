jessest94106/openairinterface5g

commit e2520c4d66f34e1a42331abdc02a682c0af28ca2

Remove dedicated Msg3 TDA
A dedicated Msg3 TDA is simply not necessary: get_feasible_msg3_tda()
already iterates through all TDAs to find a suitable one for Msg3, and
takes things such as parameter Δ already into account.

The only prerequisite is that the ra_ResponseWindow is long enough. In
some configurations, Msg3 TDA ensured that Msg2 still happened during
ra_ResponseWindow, while Msg3 was outside of it. This is not necessarily
the case anymore, unless ra_ResponseWindow is long enough.

For the normal user, removing ra_ResponseWindow from the config will be
enough.

openair2/LAYER2/NR_MAC_gNB/nr_radio_config.c
@@ -1066,8 +1066,6 @@ void nr_rrc_config_ul_tda(NR_ServingCellConfigCommon_t *scc, int min_fb_delay)
 
   const int k2 = min_fb_delay;
 
-  int mu = scc->uplinkConfigCommon->initialUplinkBWP->genericParameters.subcarrierSpacing;
-
   // UL TDA index 0 is basic slot configuration starting in symbol 0 til the last but one symbol
   NR_PUSCH_TimeDomainResourceAllocation_t *tda;
   tda = set_TimeDomainResourceAllocation(k2, get_SLIV(0, 13));
@@ -1097,20 +1095,6 @@ void nr_rrc_config_ul_tda(NR_ServingCellConfigCommon_t *scc, int min_fb_delay)
       tda = set_TimeDomainResourceAllocation(k2, sliv);
       asn1cSeqAdd(&tda_list->list, tda);
     }
-    // UL TDA index 3 for msg3 in the mixed slot (TDD)
-    int tdd_period_idx = get_tdd_period_idx(scc->tdd_UL_DL_ConfigurationCommon);
-    int nb_periods_per_frame = get_nb_periods_per_frame(tdd_period_idx);
-    int nb_slots_per_period = ((1 << mu) * 10) / nb_periods_per_frame;
-    int k2_msg3 = nb_slots_per_period - get_delta_for_k2(mu);
-    bool no_mix_slot = ul_symb < 3; // we need at least 2 symbols for scheduling Msg3
-    long sliv = no_mix_slot ? get_SLIV(0, 13) : get_SLIV(NR_NUMBER_OF_SYMBOLS_PER_SLOT - ul_symb, ul_symb - 1);
-    struct NR_PUSCH_TimeDomainResourceAllocation *puschTdrAllocMsg3 = set_TimeDomainResourceAllocation(k2_msg3, sliv);
-    if (*puschTdrAllocMsg3->k2 < min_fb_delay)
-      *puschTdrAllocMsg3->k2 += nb_slots_per_period;
-    AssertFatal(*puschTdrAllocMsg3->k2 < 33,
-                "Computed k2 for msg3 %ld is larger than the range allowed by RRC (0..32)\n",
-                *puschTdrAllocMsg3->k2);
-    asn1cSeqAdd(&tda_list->list, puschTdrAllocMsg3);
   }
 }
 

commit 2380a90a7c92b679eefb8d03e7b2044f287994c2

UL TDA: ensure we reach Msg3 in all cases
In at least the case of mu=1, min_rxtxtime=2 and DDDSU with no UL
symbols (for ULSCH) in the mixed slot, the current state of code does
not allow a UE to attach.  This is because at mu=1, Msg3 is k2+delta,
where k2=min_rxtxtime and delta(mu)=3.

Prior, this special case was handled because we had a dedicated Msg3
TDA, which in most cases is not necessary (because the mixed slot
allocates the DCI, and the following mixed slot has the UL grant). Add
code that tries to ensure that we only add the Msg3 TDA in cases when we
need it.

Fixes: e2520c4d66f3 ("Remove dedicated Msg3 TDA")

openair2/LAYER2/NR_MAC_gNB/nr_radio_config.c
@@ -1115,6 +1115,7 @@ void nr_rrc_config_ul_tda(NR_ServingCellConfigCommon_t *scc, int min_fb_delay, i
 
     int N_dl1 = p1->nrofDownlinkSlots;
     int N_ul1 = p1->nrofUplinkSlots;
+    int N_dl2 = p2 ? p2->nrofDownlinkSlots : 0;
     int N_ul2 = p2 ? p2->nrofUplinkSlots : 0;
     int tdd_period_idx = get_tdd_period_idx(scc->tdd_UL_DL_ConfigurationCommon);
     int nb_periods_per_frame = get_nb_periods_per_frame(tdd_period_idx);
@@ -1154,6 +1155,22 @@ void nr_rrc_config_ul_tda(NR_ServingCellConfigCommon_t *scc, int min_fb_delay, i
           asn1cSeqAdd(&tda_list->list, tda);
         }
       }
+    }
+
+    // for Msg3, an additional get_delta_for_k2(mu) is added to k2.
+    // check that any UL slot is reachable under this condition
+    // the below is designed to only hit specific (known) cases
+    // examples: DDDSU (no TDA in S),
+    int N_dl = max(N_dl1, N_dl2);
+    int delta = get_delta_for_k2(mu);
+    int k2_msg3 = k2 + delta;
+    if (nb_slots_per_period % k2_msg3 == 0 /* see example above */
+        && k2_msg3 > N_dl /* otherwise, can always have a D that reaches U */
+        && k2_msg3 > N_ul /* otherwise, can always have a D that reaches U */
+        && !has_ul_mixed /* if mixed slot, even for DDDSU would reach */) {
+      /* reach next UL from mixed slot in previous period */
+      tda = set_TimeDomainResourceAllocation(k2_msg3, get_SLIV(0, 13));
+      asn1cSeqAdd(&tda_list->list, tda);
     }
   }
 