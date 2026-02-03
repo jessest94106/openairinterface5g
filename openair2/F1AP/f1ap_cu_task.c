1b618f17da8b0e42a0c0c55aa61db746a2424c25
Guido Casati <hello@guidocasati.com>
2026-02-03 16:32:09 +0100

    Paging (F1AP): wire paging through mac_rrc_dl and use F1AP free/decoding lib on CU/DU
    Introduce paging_transfer in the mac_rrc_dl interface so the CU can pass
    paging to the DU (F1 or direct). Use the F1AP paging lib on both CU and DU
    for decode/free and to avoid leaking the message after send.
    
    - Add f1_paging_transfer_func_t and paging_transfer to nr_mac_rrc_dl_if_s
      (mac_rrc_dl.h, nr_rrc_defs.h).
    - F1AP path: paging_f1ap() allocates F1AP_PAGING, copies with cp_f1ap_paging(),
      sends via ITTI to TASK_CU_F1. Direct path: f1_paging_transfer_direct() stub
    - CU task: call free_f1ap_paging() after CU_send_Paging() to release the message.
    - DU: DU_handle_Paging() uses decode_f1ap_paging()/free_f1ap_paging() from lib,
      returns on decode failure; remove inline ASN.1 IE parsing.
    - cleanup outdated header comments in header files
    - Left a TODO not in DU code to build PCCH (in F1 and direct paths)

--- openair2/F1AP/f1ap_cu_task.c
+++ openair2/F1AP/f1ap_cu_task.c
@@ -38,6 +38,7 @@
 #include "lib/f1ap_interface_management.h"
 #include "lib/f1ap_ue_context.h"
 #include "f1ap_cu_paging.h"
+#include "lib/f1ap_paging.h"
 #include "f1ap_cu_task.h"
 #include "openair2/RRC/NR/nr_rrc_defs.h"
 #include <openair3/ocp-gtpu/gtp_itf.h>
@@ -204,6 +205,7 @@ void *F1AP_CU_task(void *arg) {
 
       case F1AP_PAGING:
         CU_send_Paging(assoc_id, &F1AP_PAGING(received_msg));
+        free_f1ap_paging(&F1AP_PAGING(received_msg));
         break;
 
       case F1AP_UE_CONTEXT_MODIFICATION_CONFIRM:
